2df033638a94601c4f79e17c5ca11dc93025aa88
Murat Caliskan <muratcnjid@hotmail.com>
2018-03-10 05:32:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+    if (cardNumber) {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     for (var i = cart.length; i >= 0; i--) {
       cart.pop();
